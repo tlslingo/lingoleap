c7ffc0e5db6d7c38f7037c30a038ca044bf3f46a
tlslingo <tlsmirpurkhas@gmail.com>
2026-03-29 01:15:07 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import CareerAI from './CareerAI.jsx'
+
+const path = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {path.includes('career') ? <CareerAI /> : <App />}
   </StrictMode>
 )
